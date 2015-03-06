9c15ec5e6f81a7843cd1c00a958186edb15891bc
kklasmei <kklasmei@pratt.edu>
2015-03-06 15:24:13 +0000

    add

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
 var star = "*";
 
-for (var myVar = 10; myVar <= 10; myVar++) {
+for (var myVar = 1; myVar <= 10; myVar++) {
   star = star + "*";
   console.log(star);
   var emoji = ":):):)";
 
-  for (var myVar2 = 10; myVar2 < 10; myVar2++) {
+  for (var myVar2 = 1; myVar2 < 10; myVar2++) {
     emoji = emoji + ":)";
     if (myVar2 == 10) {
       emoji = ":):):)";
